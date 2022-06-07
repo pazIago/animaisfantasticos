fd38e9d42fbd4884881d4565994adc26af327a88
pazIago <iagopaz@hotmail.com.br>
2022-06-07 16:04:26 -0300

    tooltip added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ import initTabMenu from "./modules/tabMenu.js";
 import initSmoothScrolling from "./modules/smoothScrolling.js";
 import initScrollAnimation from "./modules/scrollAnimation.js";
 import initModal from "./modules/modal.js";
-
+import initTooltip from './modules/tooltip.js'
 initAccordion();
 initTabMenu();
 initSmoothScrolling();
 initScrollAnimation();
 initModal()
+initTooltip()
